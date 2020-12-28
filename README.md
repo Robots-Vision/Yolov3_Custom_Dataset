d68626ed8b5b6e6ea24f82d25462446da42d88ab
Joel Sena <joel.sena229@gmail.com>
2020-12-28 13:07:27 -0300

    update

---
+++ README.md
@@ -0,0 +1,21 @@
+# Yolov3_Custom_Dataset
+Using a custom trained model for object detection
+
+# Instructions
+```
+pip install -r requirements.txt --no-deps --user
+```
+This command will install all required libraries for this project
+
+[Resource folder](https://drive.google.com/drive/folders/1h4WClBZur-RPh_-WznrnEzUpdFEQmeQG?usp=sharing)
+## Possíveis erros
+If appears the following error in your terminal:
+```
+'pip' is not recognized as an internal or external command,
+operable program or batch file.
+```
+You need to add pip on your PATH variable, for this you just need to execute the command below:
+```
+set PATH=%PATH%;C:\Program Files\Python37\Scripts
+```
+*Note: If your python interpreter was at other version or been in another path, modify the command.*
---
+++ yolo_object_detection.py
@@ -0,0 +1,83 @@
+# If you use VSCode
+from cv2 import cv2
+# else
+# import cv2
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+
+# Load Yolo
+net = cv2.dnn.readNetFromDarknet("cfg/yolov3_testing.cfg",r"weights/yolov3_training_last.weights")
+
+# Name custom object
+classes = ["cardboard","glass","metal","paper","plastic","trash"]
+
+
+cap = cv2.VideoCapture(0)
+print(cap)
+
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+
+# loop through them cap
+while True:
+    # Loading frame
+    ret, img = cap.read()
+
+
+    height, width = img.shape[:2]
+
+    # Detecting objects
+    blob = cv2.dnn.blobFromImage(img, 1/255.0, (416, 416), swapRB=True, crop=False)
+    net.setInput(blob)
+
+    outs = net.forward(output_layers)
+
+    # Showing informations on the screen
+    class_ids = []
+    confidences = []
+    boxes = []
+
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.3:
+                # Object detected
+                print("ID da classe:  ")
+                print(class_id)
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+
+                # Rectangle coordinates
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+    print("Index: ")
+    print(indexes)
+    font = cv2.FONT_HERSHEY_PLAIN
+    for i in range(len(boxes)):
+        if i in indexes:
+            x, y, w, h = boxes[i]
+            label = str(classes[class_ids[i]])
+            color = colors[class_ids[i]]
+            cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+            cv2.putText(img, label, (x, y + 30), font, 3, color, 2)
+
+
+    cv2.imshow("Frame", img)
+    if ord("q") == cv2.waitKey(1):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
